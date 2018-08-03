347f325c22dcced90a884b44a80c8cd4246b3343
Ace Dimasuhid <ace.dimasuhid@gmail.com>
2018-08-03 11:26:25 +0800

    Make edukasyon_style current non-destructive

--- lib/edukasyon/cli.rb
+++ lib/edukasyon/cli.rb
@@ -3,7 +3,7 @@ require 'thor'
 class CLI < Thor
   desc "current", "Run rubocop for develop"
   def current
-    exec "git diff-tree -r --no-commit-id --name-only develop@\{u\} head | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs bundle exec rubocop -a --force-exclusion"
+    exec "git diff-tree -r --no-commit-id --name-only develop@\{u\} head | xargs ls -1 2>/dev/null | grep '\.rb$' | xargs bundle exec rubocop --force-exclusion"
   end
 
   desc "default", "Run rubocop for the whole repo"
--- lib/edukasyon/style/version.rb
+++ lib/edukasyon/style/version.rb
@@ -1,5 +1,5 @@
 module Edukasyon
   module Style
-    VERSION = "0.2.3"
+    VERSION = "0.2.4"
   end
 end
